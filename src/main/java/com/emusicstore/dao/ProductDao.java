91b02850935a25111edbf8ad2fe864df9e906946
MaroM <maro.muszynski@gmail.com>
2016-03-16 22:29:57 +0100

    build Product list page using bootstrap table components, add navigation to products from home page and add navigation back to home page

--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -15,7 +15,6 @@ public class ProductDao {
     public List<Product> getProductList() {
 
         Product product1 = new Product();
-
         product1.setProductName("Guitar");
         product1.setProductCategory("Instrument");
         product1.setProductDescription("This is a fender strat guitar!");
@@ -25,8 +24,31 @@ public class ProductDao {
         product1.setUnitInStock(11);
         product1.setProductManufacturer("Fender");
 
+        Product product2 = new Product();
+        product2.setProductName("Nice Record");
+        product2.setProductCategory("Record");
+        product2.setProductDescription("This is a mix of disco polo hits!");
+        product2.setProductPrice(26);
+        product2.setProductCondition("new");
+        product2.setProductStatus("Active");
+        product2.setUnitInStock(7);
+        product2.setProductManufacturer("Polo Records");
+
+        Product product3 = new Product();
+        product3.setProductName("Speaker 2D");
+        product3.setProductCategory("Accessory");
+        product3.setProductDescription("This is a polk shelf speaker!");
+        product3.setProductPrice(433);
+        product3.setProductCondition("new");
+        product3.setProductStatus("Active");
+        product3.setUnitInStock(13);
+        product3.setProductManufacturer("Polk");
+
         productList = new ArrayList<Product>();
         productList.add(product1);
+        productList.add(product2);
+        productList.add(product3);
+
 
         return productList;
     }
