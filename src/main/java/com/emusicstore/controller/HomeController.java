eb1e68d0f9b4b134410083fce12e80098e899845
MaroM <maro.muszynski@gmail.com>
2016-03-17 22:03:35 +0100

    add edit product data (pictures as well) functionality

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -115,6 +115,36 @@ public class HomeController {
 
         return "redirect:/admin/productInventory";
     }
+
+    @RequestMapping("/admin/productInventory/editProduct/{id}")
+    public String editProduct(@PathVariable("id") String id, Model model) {
+
+        Product product = productDao.getProductById(id);
+        model.addAttribute(product);
+
+        return "editProduct";
+    }
+
+    @RequestMapping(value = "/admin/productInventory/editProduct", method=RequestMethod.POST)
+    public String editProduct(@ModelAttribute("product") Product product, Model model, HttpServletRequest request) {
+
+        MultipartFile productImage = product.getProductImage();
+        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+        path = Paths.get(rootDirectory + "/WEB-INF/resources/images/" + product.getProductId() + ".png");
+
+        if (productImage != null && !productImage.isEmpty()) {
+            try {
+                productImage.transferTo(new File(path.toString()));
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Product image file saving failed!", e);
+            }
+        }
+
+        productDao.editProduct(product);
+
+        return "redirect:/admin/productInventory";
+    }
 }
 
 
